ac021c58fbc704bb36bde298c82d11b7281a8377
OH-GITAEK <dhrlxor1512@gmail.com>
2024-09-27 01:41:41 +0900

    test cicd

--- src/main/java/net/allbareum/allbareumbackend/HelloController.java
+++ src/main/java/net/allbareum/allbareumbackend/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/test")
     public String test() {
-        return "Allbareum backend test";
+        return "Allbareum backend test gooooooooood";
     }
 }
