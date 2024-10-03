52723c8891d90c57d6518a81721e43697ab62605
OH-GITAEK <dhrlxor1512@gmail.com>
2024-10-03 16:32:56 +0900

    feat : create UserCreaeteRequestDto(#1)

---
+++ src/main/java/net/allbareum/allbareumbackend/domain/user/application/dto/UserCreateRequestDto.java
@@ -0,0 +1,14 @@
+package net.allbareum.allbareumbackend.domain.user.application.dto;
+
+import jakarta.persistence.Column;
+import lombok.Getter;
+
+@Getter
+public class UserCreateRequestDto {
+
+    private String email;
+    private String password;
+    private String username;
+    private String Nickname;
+    private String role;
+}
