d20c6cba7fcdb494f16486427acdb036303699b8
OH-GITAEK <dhrlxor1512@gmail.com>
2024-10-29 23:03:43 +0900

    feat : Creaete Feedback Controller(#12)

--- src/main/java/net/allbareum/allbareumbackend/domain/feedback/presentation/FeedbackController.java
+++ src/main/java/net/allbareum/allbareumbackend/domain/feedback/presentation/FeedbackController.java
@@ -1,2 +1,24 @@
-package net.allbareum.allbareumbackend.domain.feedback.presentation;public class FeedbackController {
+package net.allbareum.allbareumbackend.domain.feedback.presentation;
+
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/feedback")
+@RequiredArgsConstructor
+public class FeedbackController {
+
+    private final FeedbackApplicationService feedbackApplicationService;
+
+    @PostMapping("/create")
+    @Operation(summary = "피드백 생성")
+    public FeedbackResponse signUp(@RequestBody @Valid FeedbackCreateRequestDto feedbackCreateRequestDto) {
+        FeedbackResponse feedbackResponse = feedbackApplicationService.create(feedbackCreateRequestDto);
+        return feedbackResponse;
+    }
 }
