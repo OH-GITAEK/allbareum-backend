4a8a4e65d89ad66b57cd61b1e146d47cba7324ed
OH-GITAEK <dhrlxor1512@gmail.com>
2024-11-01 10:25:42 +0900

    fix : jwt tocken 유지시간 변경

--- src/main/java/net/allbareum/allbareumbackend/domain/user/domain/UserService.java
+++ src/main/java/net/allbareum/allbareumbackend/domain/user/domain/UserService.java
@@ -51,7 +51,7 @@ public class UserService {
             throw new CustomException(ErrorCode.USER_WRONG_PASSWORD);
         }
 
-        String accessToken = jwtUtil.createJwt(user.getId(), user.getRole(), 60*60*1000L);
+        String accessToken = jwtUtil.createJwt(user.getId(), user.getRole(), 60L * 24 * 60 * 60 * 1000);
         System.out.println("Generated JWT: " + accessToken);
         return new UserLogInResponseDto(accessToken);
     }
