bad15ee1e1ecb6a7907c16000582f532b144d1ee
OH-GITAEK <dhrlxor1512@gmail.com>
2024-11-05 00:37:09 +0900

    fix : fix feedback entity (#12)

--- src/main/java/net/allbareum/allbareumbackend/domain/feedback/domain/Feedback.java
+++ src/main/java/net/allbareum/allbareumbackend/domain/feedback/domain/Feedback.java
@@ -24,10 +24,12 @@ public class Feedback {
     private String textSentence; // 프론트에서 전달받은 텍스트 문장
 
     private String transcription;
+    @Column(columnDefinition = "TEXT")
     private String pronunciation_feedback;
     private Double pronunciation_score;
     private String pronunciation_feedback_image;
 
+    @Column(columnDefinition = "TEXT")
     private String intonation_feedback;
     private String intonation_feedback_image;
 
