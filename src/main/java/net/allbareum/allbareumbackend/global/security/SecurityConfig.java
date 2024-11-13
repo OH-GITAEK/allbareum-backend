2f007ad818da034b61ee8e16045ff736d8e21b77
OH-GITAEK <dhrlxor1512@gmail.com>
2024-11-13 12:32:05 +0900

    actuator 의존성 추가

--- src/main/java/net/allbareum/allbareumbackend/global/security/SecurityConfig.java
+++ src/main/java/net/allbareum/allbareumbackend/global/security/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/actuator/prometheus","/", "/api/v1/users/login","/api/v1/users/sign-up", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/login", "/actuator/**","/", "/api/v1/users/login","/api/v1/users/sign-up", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated());
 
         http
